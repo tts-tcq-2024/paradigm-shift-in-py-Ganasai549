985d96e3f83672599c18f95df233cd8d1fc727cf
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-29 20:46:39 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,7 +3,7 @@ def battery_is_ok(temperature):
 
 def Check_temp(temperature):
   if temperature < 0 or temperature > 45:
-    low_high_temp(temperature)
+    low_high_temperature(temperature)
     print('Temperature is out of range!')
     return False
   else:
