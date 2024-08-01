40b50abd8239a8f58c0047143a18e1664b138ade
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-08-01 11:51:37 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,11 +1,64 @@
-def battery_is_ok(temperature):
- Check_temp(temperature)
-
-def Check_temp(temperature):
+def battery_is_ok(temperature, soc, ch_rate):
+  Checking_temp(temperature)
+  Checking_soc(soc)
+  Checking_Chargerate(ch_rate)
+  
+  def Checking_temperature(temperature):
   if temperature < 0 or temperature > 45:
     high_low_temperature(temperature)
     print('Temperature is out of range!')
     return False
   else:
-    Check_temp(temperature)
- 
+    Checking_temp(temperature)
+ def high_low_temperature(temperature):
+  if temp >0:
+    print('Temperature is High!')
+  else:
+    print('Temperature is Low.')
+
+def Checking_temperature(temperature):
+  if temp <=2.25 or temp >42.75:
+    Warning_Checking_temperature(temperature)
+  else:
+    print('Temperature is NORMAL.')
+def Warning_Checking_temperature(temperature):
+  if temp <=2.25:
+    print('State of Charge is Low Warning!')
+  else:
+    print('State of Charge is High Warning!')
+
+  def Checking_soc(soc):
+  if soc <=20 or soc > 80:
+    high_low_soc(soc)
+  else:
+    Checking_soc(soc)
+def high_low_soc(soc):
+  if soc>20:
+    print('State of Charge is High breach!')
+  else:
+    print('State of Charge is low breach!')
+def Checking_soc(soc):
+  if soc <25 or soc >75:
+    WarningCheck_soc(soc)
+  else:
+    print('State of Charge is NORMAL.')
+def WarningCheck_soc(soc):
+  if soc <25:
+    print('State of Charge is Low Warning!')
+  else:
+    print('State of Charge is High Warning!')
+def Checking_Chargerate(ch_rate):
+  if ch > 0.8:
+    print('Charge rate is out of range!')
+  else:
+    Warning_charge_rate(ch_rate)
+
+def Warning_charge_rate(ch_rate):
+  if ch> 0.6:
+    print('Charge rate is High Warning!')
+  else:
+    print('Charge rate is Normal.')
+    
+if __name__ == '__main__':
+  battery_is_ok(25, 70, 0.7)
+  battery_is_ok(50, 85, 0)
